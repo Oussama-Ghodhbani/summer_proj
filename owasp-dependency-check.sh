fe9b9764dd6eb6d49898da2cf648253c68c686e7
Oussama-Ghodhbani <90413236+Oussama-Ghodhbani@users.noreply.github.com>
2023-11-29 02:25:20 +0100

    Update owasp-dependency-check.sh

--- owasp-dependency-check.sh
+++ owasp-dependency-check.sh
@@ -1,29 +1,48 @@
-#!/bin/sh
+#!/bin/bash -e
+
+VERSION=$(mvn -q \
+    -Dexec.executable="echo" \
+    -Dexec.args='${project.version}' \
+    --non-recursive \
+    org.codehaus.mojo:exec-maven-plugin:1.3.1:exec)
+
+SCAN_TARGET="./cli/target/release/lib"
+
+if [ ! -d "$SCAN_TARGET" ]; then 
+  echo "Scan target does not exist: $SCAN_TARGET"
+  exit 1
+fi
 
 OWASPDC_DIRECTORY=$HOME/OWASP-Dependency-Check
 DATA_DIRECTORY="$OWASPDC_DIRECTORY/data"
 REPORT_DIRECTORY="$OWASPDC_DIRECTORY/reports"
+CACHE_DIRECTORY="$OWASPDC_DIRECTORY/data/cache"
 
 if [ ! -d "$DATA_DIRECTORY" ]; then
-    echo "Initially creating persistent directories"
+    echo "Initially creating persistent directory: $DATA_DIRECTORY"
     mkdir -p "$DATA_DIRECTORY"
-    chmod -R 777 "$DATA_DIRECTORY"
+fi
 
+if [ ! -d "$REPORT_DIRECTORY" ]; then
+    echo "Initially creating persistent directory: $REPORT_DIRECTORY"
     mkdir -p "$REPORT_DIRECTORY"
-    chmod -R 777 "$REPORT_DIRECTORY"
+fi
+
+if [ ! -d "$CACHE_DIRECTORY" ]; then
+    echo "Initially creating persistent directory: $CACHE_DIRECTORY"
+    mkdir -p "$CACHE_DIRECTORY"
+fi
+
+if [ -f "$HOME/OWASP-Dependency-Check/reports/dependency-check-report.json" ]; then
+    rm "$HOME/OWASP-Dependency-Check/reports/dependency-check-report.json"
 fi
 
 # Make sure we are using the latest version
-docker pull owasp/dependency-check
+# docker pull owasp/dependency-check
 
-docker run --rm \
-    --volume $(pwd):/src \
+docker run -it --rm \
     --volume "$DATA_DIRECTORY":/usr/share/dependency-check/data \
     --volume "$REPORT_DIRECTORY":/report \
-    owasp/dependency-check \
-    --scan /src \
-    --format "ALL" \
-    --project "My OWASP Dependency Check Project" \
-    --out /report
-    # Use suppression like this: (/src == $pwd)
-    # --suppression "/src/security/dependency-check-suppression.xml"
+    --entrypoint /bin/sh \
+    owasp/dependency-check:$VERSION
+    
