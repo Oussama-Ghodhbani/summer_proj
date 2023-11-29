4c910b1dcc6317ba7ab0c2f4ea216526eb3d5579
Oussama-Ghodhbani <90413236+Oussama-Ghodhbani@users.noreply.github.com>
2023-11-29 01:41:37 +0100

    Update owasp-dependency-check.sh

--- owasp-dependency-check.sh
+++ owasp-dependency-check.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
 DC_VERSION="latest"
-DC_DIRECTORY=$HOME/OWASP-Dependency-Check
 DC_PROJECT="dependency-check scan: $(pwd)"
+DC_DIRECTORY="$HOME/dependency-check-data"  # Updated data directory
 DATA_DIRECTORY="$DC_DIRECTORY/data"
-CACHE_DIRECTORY="$DC_DIRECTORY/data/cache"
+CACHE_DIRECTORY="$DATA_DIRECTORY/cache"
 
 if [ ! -d "$DATA_DIRECTORY" ]; then
     echo "Initially creating persistent directory: $DATA_DIRECTORY"
@@ -23,11 +23,13 @@ docker run --rm \
     -u $(id -u ${USER}):$(id -g ${USER}) \
     --volume $(pwd):/src:z \
     --volume "$DATA_DIRECTORY":/usr/share/dependency-check/data:z \
-    --volume $(pwd)/odc-reports:/report:z \
+    --volume $(pwd)/reports:/report:z \
     owasp/dependency-check:$DC_VERSION \
     --scan /src \
     --format "ALL" \
     --project "$DC_PROJECT" \
-    --out /report
-    # Use suppression like this: (where /src == $pwd)
-    # --suppression "/src/security/dependency-check-suppression.xml"
+    --out /report \
+    --cveUrl12Modified "https://nvd.nist.gov/feeds/json/cve/1.2/nvdcve-1.2-modified.json.gz"  # Added custom CVE URL
+
+# Use suppression like this: (where /src == $pwd)
+# --suppression "/src/security/dependency-check-suppression.xml"
